0f2a18cc7bac13cdb0d23a23d0ced28f0183d338
JOBLIN <tjobl10@eq.edu.au>
2021-09-15 15:57:49 +1000

    Placing towers
    You can click on a tile and it will place a red square there. That red square is the instance of a class. This means we can put logic in it and we can call it in a loop every frame.

---
+++ empty-example/sketch.js
@@ -0,0 +1,61 @@
+let towers = {}
+let towergraphics
+let someconstant 
+
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  background(0)
+  console.log("bruh")
+  towergraphics = createGraphics(windowWidth, windowHeight)
+  if (windowWidth-300 % 9 > windowHeight & 5) {someconstant = (windowWidth-300)/9} else {someconstant = windowHeight / 5}
+  console.log(someconstant)
+}
+
+function windowResized() {
+  resizeCanvas (windowWidth, windowHeight)
+  towergraphics = createGraphics(windowWidth, windowHeight)
+  towergraphics.clear()
+  for (f in towers) {
+    towers[f].draw()
+  }
+}
+
+function draw() {
+  background(0)
+  noFill()
+  image(towergraphics, 0, 0)
+  stroke(100)
+  if (mouseX < 9*someconstant && mouseY < 5*someconstant) {
+    rect ((floor(mouseX / someconstant) * someconstant),
+    (floor(mouseY / someconstant) * someconstant), someconstant, someconstant)  
+  }
+}
+
+
+function mouseClicked() {
+  if (mouseX < 9 * someconstant && mouseY < 5 * someconstant) {
+    let x = floor(mouseX /100)
+    let y =  floor(mouseY/100)
+    
+    if (!([x, y] in towers)) {
+      towers[[x, y]] = new tower(x, y)
+    }
+  }
+  towergraphics.clear()
+  for (f in towers) {
+    towers[f].draw()
+  }
+} 
+
+
+
+class tower {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  draw() {
+    towergraphics.fill(100, 23, 43)
+    towergraphics.rect(this.x * someconstant, this.y * someconstant, someconstant, someconstant)
+  }
+}
